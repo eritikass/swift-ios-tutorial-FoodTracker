8798b878a265dddf599c50681e948c42ecb88191
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2017-05-06 01:59:51 +0300

    init add

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Siim Tiilen on 06/05/2017.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
