58c1dc16d1761ea50b196c9b1612f71f35ed5d85
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2017-05-07 17:09:44 +0300

    add RatingControl.swift

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Siim Tiilen on 07/05/2017.
+//  Copyright © 2017 Siim Tiilen. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
