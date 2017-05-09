773e6fb20fec7967a47df1ef3ccb785730eddb8c
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2017-05-10 01:43:15 +0300

    add custom ui and buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -3,13 +3,36 @@
 //  FoodTracker
 //
 //  Created by Siim Tiilen on 07/05/2017.
-//  Copyright © 2017 Siim Tiilen. All rights reserved.
 //
 
 import UIKit
 
 class RatingControl: UIStackView {
-
     
+    //MARK: Initialization
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        let button = UIButton()
+        // TODO: use html color :D
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        addArrangedSubview(button)
+    }
 }
