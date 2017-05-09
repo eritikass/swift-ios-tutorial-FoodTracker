2a0e300fe38843fe880e999b6ea8c4e962246294
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2017-05-10 02:57:27 +0300

    sync with example project

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,6 +10,7 @@ import UIKit
 @IBDesignable class RatingControl: UIStackView {
     
     //MARK: Properties
+    
     private var ratingButtons = [UIButton]()
     
     var rating = 0 {
@@ -23,7 +24,7 @@ import UIKit
             setupButtons()
         }
     }
-    
+
     @IBInspectable var starCount: Int = 5 {
         didSet {
             setupButtons()
@@ -36,10 +37,29 @@ import UIKit
         super.init(frame: frame)
         setupButtons()
     }
-    
+
     required init(coder: NSCoder) {
         super.init(coder: coder)
         setupButtons()
+    }
+    
+    //MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     
@@ -91,44 +111,19 @@ import UIKit
         updateButtonSelectionStates()
     }
     
-    //MARK: Button Action
-    
-    func ratingButtonTapped(button: UIButton) {
-        
-        guard let index = ratingButtons.index(of: button) else {
-            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
-        }
-        
-        // Calculate the rating of the selected button
-        let selectedRating = index + 1
-        
-        if selectedRating == rating {
-            // If the selected star represents the current rating, reset the rating to 0.
-            rating = 0
-        } else {
-            // Otherwise set the rating to the selected star
-            rating = selectedRating
-        }
-        
-        //button.backgroundColor = UIColor.blue
-        print("Button pressed 👍")
-        print(selectedRating)
-    }
-    
     private func updateButtonSelectionStates() {
         for (index, button) in ratingButtons.enumerated() {
             // If the index of a button is less than the rating, that button should be selected.
             button.isSelected = index < rating
             
-            // Set the hint string for the currently selected star
+            // Set accessibility hint and value
             let hintString: String?
             if rating == index + 1 {
                 hintString = "Tap to reset the rating to zero."
             } else {
                 hintString = nil
             }
-            
-            // Calculate the value string
+
             let valueString: String
             switch (rating) {
             case 0:
@@ -139,7 +134,6 @@ import UIKit
                 valueString = "\(rating) stars set."
             }
             
-            // Assign the hint string and value string
             button.accessibilityHint = hintString
             button.accessibilityValue = valueString
         }
